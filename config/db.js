d44ecbdf356774806b29eb38ad036f9f50ba7c02
zeeshan-maker <zeeshankhan956843@gmail.com>
2025-07-29 22:14:27 +0530

    add database

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
- {
-  host: process.env.DB_HOST, // Database host
-  dialect: process.env.DB_dialect, // Use PostgreSQL as the database dialect
-  username:process.env.DB_USER , // Your PostgreSQL username
-  password: process.env.DB_PASSWORD, // Your PostgreSQL password
-  database: process.env.DB_NAME, // Your PostgreSQL database name
-  
-}
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+   dialect: process.env.DB_dialect,
+   dialectOptions: {
+      ssl: {
+        require: true,              // Render ke liye required
+        rejectUnauthorized: false   // Self-signed certs allow
+      }
+    }
+  }
+
 );
 
 module.exports = sequelize;
